0ebe042f488fd4ea4679c50c38c36f99119354af
Izaak Meckler <ihmeckler@gmail.com>
2019-06-09 17:12:08 -0700

    fix build script

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 mkdir build
 pushd build
   cmake -DMULTICORE=ON -DUSE_PT_COMPRESSION=OFF .. 
-  make -j12 main generate_inputs generate_parameters
+  make -j12 main generate_parameters
 popd
 mv build/libsnark/main .
-mv build/libsnark/generate_inputs .
 mv build/libsnark/generate_parameters .
