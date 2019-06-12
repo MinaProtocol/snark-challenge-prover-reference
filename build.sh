97e2115a8fc6c7185190cc559d3f346294eecd91
Corey Richardson <cmr@o1labs.org>
2019-06-11 18:01:42 -0700

    build cuda_bench by default

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 mkdir build
 pushd build
   cmake -DMULTICORE=ON -DUSE_PT_COMPRESSION=OFF .. 
-  make -j12 main generate_parameters
+  make -j12 main generate_parameters cuda_bench
 popd
 mv build/libsnark/main .
 mv build/libsnark/generate_parameters .
