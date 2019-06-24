9070296985dae95e6ccef46467486ba86631ba18
Corey Richardson <cmr@o1labs.org>
2019-06-24 14:19:13 -0700

    build cuda_prover_piecewise with build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 mkdir build
 pushd build
   cmake -DMULTICORE=ON -DUSE_PT_COMPRESSION=OFF .. 
-  make -j12 main generate_parameters
+  make -j12 main generate_parameters cuda_prover_piecewise
 popd
 mv build/libsnark/main .
 mv build/libsnark/generate_parameters .
+mv build/cuda_prover_piecewise .
