8d9519ed7fbb58e1bf05b62dfec7d54ef9014b8e
nginnever <ginneversource@gmail.com>
2019-07-23 19:55:27 -0700

    delete cuda, init opencl

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 mkdir build
 pushd build
   cmake -DMULTICORE=ON -DUSE_PT_COMPRESSION=OFF $EXTRA_CMAKE_ARGS_FOR_CI ..
-  make -j12 main generate_parameters cuda_prover_piecewise
+  make -j12 main generate_parameters
 popd
 mv build/libsnark/main .
 mv build/libsnark/generate_parameters .
-mv build/cuda_prover_piecewise .
